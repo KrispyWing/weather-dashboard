07c4a3ba28c4684e8835e96b00474a358e9f4b4b
Kris Pennimpede <kpennimpede@hotmail.com>
2021-01-17 17:50:31 -0500

    setup two fetch requests to get all necessary weather data

--- assets/script.js
+++ assets/script.js
@@ -4,9 +4,23 @@ var getWeatherData = function(city) {
   fetch(apiUrl).then(function(response) {
     response.json().then(function(data) {
       console.log(data);
+      var lat = data.coord.lat;
+      var lon = data.coord.lon;
+      getFiveDay(lat, lon);
     })
   })
   
 }
 
+var getFiveDay = function (lat, lon) {
+  var apiUrl = "https://api.openweathermap.org/data/2.5/onecall?lat="+ lat + "&lon="+ lon + "&exclude=hourly,minutely,alerts&appid=aba5ba8bf6ca29b364deececed3fe28b";
+
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function(data) {
+      console.log(data);
+    })
+  })
+}
+
 getWeatherData("Toronto");
+
